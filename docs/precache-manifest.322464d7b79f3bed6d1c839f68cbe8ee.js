722909b05ed3b7eb3f16f1ab29249d56c96e5dd3
Pedro Medrano <petermedrano@hotmail.com>
2021-03-31 15:48:48 -0500

    Paginas

---
+++ docs/precache-manifest.322464d7b79f3bed6d1c839f68cbe8ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d1a46adeeec7b31248c75437052fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ffb248cfa7241b1dbda",
+    "url": "/static/css/main.e344c27a.chunk.css"
+  },
+  {
+    "revision": "7a20b51d34961c30974c",
+    "url": "/static/js/2.6606d5d4.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.6606d5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffb248cfa7241b1dbda",
+    "url": "/static/js/main.1a8d8369.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
